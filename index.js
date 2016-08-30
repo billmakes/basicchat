316d8d8428dfb16700c2717ec6f67a316c0df1d5
Bill Hilbert <hilbertwilliam@gmail.com>
2016-08-30 00:10:57 -0400

    heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var server = require('http').createServer(app);
+var port = process.env.PORT || 3700;
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
@@ -8,20 +8,15 @@ app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res){
-    res.render("page");
+	res.render("page");
 });
 
-app.get("/", function(req, res){
-    res.send("It works!");
-});
-
-var io = require('socket.io').listen(server);
+var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
-    socket.on('send', function (data) {
-        io.sockets.emit('message', data);
-    });
+	socket.emit('message', { message: 'welcome to the chat' });
+	socket.on('send', function (data) {
+		io.sockets.emit('message', data);
+	});
 });
-
-server.listen(process.env.PORT || 3000);
+console.log("Listening on port " + port);
